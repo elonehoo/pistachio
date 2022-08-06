f59a972bf763aa03f3218d31a6ca7d6cf0b8948f
xiaoxunyao <huchengyea@163.com>
2022-08-06 21:39:42 +0800

    fix: update util

--- package/core/src/utils/index.ts
+++ package/core/src/utils/index.ts
@@ -1,5 +1,18 @@
 import { ref, isRef,Ref} from 'vue'
 
 export type RefTyped<T> = T | Ref<T>
+export type RefElement = Element | Ref<Element | undefined>
+export type WrapRef<T> = T extends Ref<any> ? T : Ref<T>
 
+export function unwrap<T>(value: RefTyped<T>): T
+export function unwrap<T>(value: RefTyped<T>): T {
+  return isRef(value) ? value.value : value;
+}
 
+export function wrap(o: RefElement): Ref<Element>
+export function wrap<T>(o: RefTyped<T>): Ref<T>
+export function wrap(o: any): any {
+  return isRef(o) ? o : ref(o)
+}
+
+export const isArray = Array.isArray
