6e2d89d00c5888a8fb6a763bfdccbb2543f1cb2a
xiaoxunyao <huchengyea@163.com>
2022-08-07 20:49:18 +0800

    chore: init vite config

---
+++ test/vite.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+  ],
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  },
+})
