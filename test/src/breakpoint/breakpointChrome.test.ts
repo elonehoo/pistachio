a354abe129ea22a59cb21b19c51cce848df9a996
xiaoxunyao <huchengyea@163.com>
2022-08-17 18:47:50 +0800

    fix: update break point chrome

---
+++ test/src/breakpoint/breakpointChrome.test.ts
@@ -0,0 +1,22 @@
+import { beforeAll, beforeEach, describe, expect, it, vi } from 'vitest'
+import { useBreakpointChrome, useBreakpoint } from '@elonehoo/vue-hooks'
+
+describe.skip("breakpointChrome", () => {
+  it("should call breakpoint with chrome breakpoints", () => {
+    const expected = {
+      a: 1
+    };
+
+    expect(useBreakpointChrome()).toBe(expected);
+
+    expect(useBreakpoint).toHaveBeenCalledWith({
+      mobileS: 320,
+      mobileM: 375,
+      mobileL: 425,
+      tablet: 768,
+      laptop: 1024,
+      laptopL: 1440,
+      desktop4K: 2560
+    });
+  });
+});
