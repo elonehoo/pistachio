7c2e763fbb82c15733fe71019a5932910030c019
xiaoxunyao <huchengyea@163.com>
2022-08-18 09:07:30 +0800

    fix: update interval

--- packages/core/src/misc/index.ts
+++ packages/core/src/misc/index.ts
@@ -1,2 +1,3 @@
 export * from './matchMedia'
 export * from './model'
+export * from './interval'
---
+++ test/src/misc/interval.test.ts
@@ -0,0 +1,154 @@
+import { describe, expect, it,beforeEach,afterAll,vi } from 'vitest'
+import { createVue } from '../utils'
+import { useInterval, UseIntervalReturn, UseIntervalReturnMs, UseIntervalReturnArgs } from '@elonehoo/vue-hooks'
+
+describe.skip("interval", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+  it("should work", async () => {
+    const callback = vi.fn();
+    const ms = 100;
+
+    let interval: UseIntervalReturn & UseIntervalReturnMs = {} as any;
+    const { mount, destroy } = createVue({
+      template: `<div></div>`,
+      setup() {
+        interval = useInterval(callback, ms);
+      }
+    });
+
+    mount();
+
+    expect(clearInterval).not.toHaveBeenCalled();
+    expect(setInterval).toHaveBeenCalledWith(callback, ms);
+
+    interval.start();
+    expect(clearInterval).toHaveBeenCalled();
+    expect(setInterval).toHaveBeenNthCalledWith(2, callback, ms);
+
+    interval.remove();
+
+    expect(clearInterval).toHaveBeenCalledTimes(2);
+
+    interval.start();
+
+    destroy();
+
+    expect(clearInterval).toHaveBeenCalledTimes(3);
+  });
+
+  it("should remove at unmount", () => {
+    const callback = vi.fn();
+    const ms = 100;
+
+    const { mount, destroy } = createVue({
+      template: `<div></div>`,
+      setup() {
+        useInterval(callback, ms);
+      }
+    });
+
+    mount();
+
+    expect(clearInterval).not.toHaveBeenCalled();
+    expect(setInterval).toHaveBeenCalledWith(callback, ms);
+
+    destroy();
+
+    expect(clearInterval).toHaveBeenCalled();
+  });
+
+  it("should not start if ms are not passed", () => {
+    const callback = vi.fn();
+    const ms = 100;
+
+    let interval: UseIntervalReturn = {} as any;
+    const { mount, destroy } = createVue({
+      template: `<div></div>`,
+      setup() {
+        interval = useInterval(callback);
+      }
+    });
+
+    mount();
+
+    expect(clearInterval).not.toHaveBeenCalled();
+    expect(setInterval).not.toHaveBeenCalled();
+
+    interval.start(ms);
+    expect(setInterval).toHaveBeenCalledWith(callback, ms);
+
+    destroy();
+  });
+
+  it("should override ms on start()", () => {
+    const callback = vi.fn();
+    const ms = 100;
+
+    let interval: UseIntervalReturn & UseIntervalReturnMs = {} as any;
+    const { mount, destroy } = createVue({
+      template: `<div></div>`,
+      setup() {
+        interval = useInterval(callback, ms);
+      }
+    });
+
+    mount();
+
+    expect(clearInterval).not.toHaveBeenCalled();
+    expect(setInterval).toHaveBeenCalled();
+
+    interval.start(20);
+    expect(setInterval).toHaveBeenLastCalledWith(callback, 20);
+
+    destroy();
+  });
+
+  it("should override args on start()", () => {
+    const callback = vi.fn((x: number) => x);
+    const ms = 100;
+
+    let interval: UseIntervalReturn &
+      UseIntervalReturnArgs<[number]> = {} as any;
+    const { mount, destroy } = createVue({
+      template: `<div></div>`,
+      setup() {
+        interval = useInterval(callback, ms, 1);
+      }
+    });
+
+    mount();
+
+    expect(clearInterval).not.toHaveBeenCalled();
+    expect(setInterval).toHaveBeenLastCalledWith(callback, ms, 1);
+
+    interval.start(undefined, 2);
+    expect(setInterval).toHaveBeenLastCalledWith(callback, ms, 2);
+
+    destroy();
+  });
+
+  it("should not start if no ms passed", () => {
+    const callback = vi.fn((x: number) => x);
+    let interval: UseIntervalReturnMs = {} as any;
+    const { mount, destroy } = createVue({
+      template: `<div></div>`,
+      setup() {
+        // @ts-ignore
+        interval = useInterval(callback);
+      }
+    });
+
+    mount();
+
+    expect(clearInterval).not.toHaveBeenCalled();
+    interval.start();
+    expect(setInterval).not.toHaveBeenCalled();
+
+    destroy();
+  });
+});
