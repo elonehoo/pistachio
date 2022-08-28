e4056958a965fbbddc4aba08558c244d624d6a3e
xiaoxunyao <huchengyea@163.com>
2022-08-28 13:26:15 +0800

    fix: update storage test

--- packages/core/src/storage/index.ts
+++ packages/core/src/storage/index.ts
@@ -1,3 +1,4 @@
 export * from './webStorage'
 export * from './localStorage'
 export * from './sessionStorage'
+export * from './storage'
---
+++ test/src/storage/storage.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest'
+import { useStorage } from '@elonehoo/vue-hooks'
+
+describe("storage", () => {
+  it("should get localStorage if supported", () => {
+    const { storage } = useStorage("");
+
+    expect(storage).not.toBeNull();
+  });
+});
