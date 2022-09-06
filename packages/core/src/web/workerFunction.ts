44ecac138350635128e724cc149fddd8ddc035d8
elonehoo <huchengyea@163.com>
2022-09-06 09:30:32 +0800

    chore: init worker function api

---
+++ packages/core/src/web/workerFunction.ts
@@ -0,0 +1,133 @@
+import { computed, isRef, watch } from 'vue'
+import type { RefTyped } from '../utils'
+import { NO_OP, PASSIVE_EV, isClient, unwrap } from '../utils'
+import type { CancellablePromiseResult, PromiseResultFactory } from '../promise'
+import { useCancellablePromise } from '../promise'
+
+export const inlineWorkExecution = (f: Function) =>
+  function (e: MessageEvent) {
+    const args = e.data || []
+
+    return new Promise((res) => {
+      try {
+        Promise.resolve(f.apply(f, args))
+          .then(x => res(postMessage([true, x])))
+          .catch(x => res(postMessage([false, x])))
+      }
+      catch (e) {
+        res(postMessage([false, e]))
+      }
+    })
+  }
+
+export function createBlobUrl(fn: Function, dependencies: readonly string[]) {
+  const scripts
+    = dependencies.length > 0
+      ? `importScripts("${dependencies.join('","')}");`
+      : ''
+
+  const blobScript = [
+    scripts,
+    'onmessage=',
+    `(${inlineWorkExecution.toString()})(${fn.toString()})`,
+  ]
+
+  const blob = new Blob(blobScript, { type: 'text/javascript' })
+  return URL.createObjectURL(blob)
+}
+
+export interface WebWorkerFunctionOptions {
+  dependencies?: RefTyped<string[]>
+  timeout?: RefTyped<number>
+}
+
+export function useWorkerFunction<T, TArgs extends Array<any>>(
+  fn: (...args: TArgs) => T,
+  options?: WebWorkerFunctionOptions,
+): PromiseResultFactory<Promise<T | undefined>, TArgs> &
+  CancellablePromiseResult {
+  const supported = isClient && 'Worker' in self
+  // reactive
+  const dependencies = computed(
+    () => (options && unwrap(options.dependencies)) || [],
+  )
+  const timeoutRef = computed(() => options && unwrap(options.timeout))
+
+  if (!supported)
+    return useCancellablePromise(fn, { lazy: true, throwException: true })
+
+  const promise = useCancellablePromise<T | undefined, TArgs>(
+    (...args: TArgs) =>
+      new Promise<T | undefined>((res, rej) => {
+        const blobUrl = createBlobUrl(fn, dependencies.value)
+        const worker = new Worker(blobUrl)
+
+        let timeoutId = -1
+        const timeout = timeoutRef.value
+
+        const terminate = () => {
+          worker.terminate()
+          URL.revokeObjectURL(blobUrl)
+          clearTimeout(timeoutId)
+          removeWatch()
+        }
+
+        // if the last argument is ref(false) we should also track it
+        const watchCancel
+          = args.length === fn.length + 1
+          && isRef(args[args.length - 1])
+          && args[args.length - 1].value === false
+            ? computed(
+              () => promise.cancelled.value || args[args.length - 1].value,
+            )
+            : promise.cancelled
+
+        const removeWatch = watchCancel
+          ? watch(
+            watchCancel as any,
+            () => {
+              terminate()
+              res(undefined)
+            },
+            { immediate: false },
+          )
+          : NO_OP
+
+        worker.addEventListener(
+          'message',
+          (e) => {
+            if (e.data[0])
+              res(e.data[1])
+            else
+              rej(e.data[1])
+
+            terminate()
+          },
+          PASSIVE_EV,
+        )
+
+        worker.addEventListener(
+          'error',
+          (e) => {
+            terminate()
+            rej(e)
+          },
+          PASSIVE_EV,
+        )
+
+        worker.postMessage([...args])
+
+        if (timeout) {
+          timeoutId = setTimeout(() => {
+            promise.cancel(`[WebWorker] timeout after ${timeout}ms`)
+          }, timeout) as any
+        }
+      }),
+    {
+      lazy: true,
+      throwException: true,
+    },
+  )
+
+  return promise
+}
