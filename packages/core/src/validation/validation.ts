abb2bfb1ada640ab2cb2a6fe6ef15f65aae0b079
elonehoo <huchengyea@163.com>
2022-09-06 16:38:10 +0800

    chore: init validation api and test

---
+++ packages/core/src/validation/validation.ts
@@ -0,0 +1,408 @@
+import type { Ref, UnwrapRef } from 'vue'
+import { computed, reactive, ref, watch } from 'vue'
+import type { RefTyped } from '../utils'
+import { NO_OP, isBoolean, isObject, isPromise, unwrap, wrap } from '../utils'
+
+type ValidatorFunc<T, TContext = any> = (
+  model: T,
+  ctx: TContext
+) => boolean | Promise<boolean>
+
+interface ValidatorObject<TValidator extends ValidatorFunc<any>> {
+  $validator: TValidator
+  $message: RefTyped<string>
+}
+
+type Validator<T> = ValidatorFunc<T> | ValidatorObject<ValidatorFunc<T>>
+
+interface ValidationValue<T> {
+  $value: UnwrapRef<T>
+  $dirty: boolean
+  $errors: Array<any>
+  $anyInvalid: boolean
+
+  $touch(): void
+  $reset(): void
+}
+
+interface ValidatorResult {
+  $error: any
+  $invalid: boolean
+}
+
+interface ValidationGroupResult {
+  $anyDirty: boolean
+  $errors: Array<any>
+  $anyInvalid: boolean
+
+  $touch(): void
+  $reset(): void
+}
+
+interface ValidatorResultPromise {
+  $pending: boolean
+  $promise: Promise<boolean> | null
+}
+
+interface ValidatorResultMessage {
+  $message: string
+}
+
+/*  Input */
+type ValidationInputType<T, TValue> = Record<
+  Exclude<keyof T, '$value'>,
+  Validator<UnwrapRef<TValue>>
+> & { $value: TValue }
+
+type ValidationInput<T> = T extends { $value: infer TValue }
+  ? ValidationInputType<T, TValue>
+  : { [K in keyof T]: ValidationInput<T[K]> }
+
+type UseValidation<T> = { [K in keyof T]: ValidationInput<T[K]> }
+
+/* /Input */
+
+/* Output */
+
+type ValidatorOutput<T, TValue> = T extends (
+  value?: TValue,
+  ctx?: any
+) => boolean
+  ? ReturnType<T> extends Promise<boolean>
+    ? ValidatorResultPromise & ValidatorResult
+    : ValidatorResult
+  : T extends { $validator: Function }
+    ? ValidatorOutput<T['$validator'], TValue> &
+    (T extends { $message: infer TM } ? { $message: TM } : {})
+    : T extends (...args: any) => infer TReturn
+      ? TReturn extends Promise<any>
+        ? ValidatorResultPromise & ValidatorResult
+        : ValidatorResult
+      : never
+
+type NonDollarSign<T> = T extends `$${infer _}` ? never : T
+
+type ToObjectOutput<T extends Record<string, any>> = T extends {
+  $value: infer V
+}
+  ? UnwrapRef<V>
+  : {
+      [K in NonDollarSign<keyof T>]: ToObjectOutput<T[K]>;
+    }
+
+type Validation<T extends Record<string, any>> = T extends {
+  $value: infer TV
+}
+  ? {
+      [K in keyof Omit<T, '$value'>]: K extends `$${infer _}`
+        ? UnwrapRef<T[K]>
+        : ValidatorOutput<T[K], TV>;
+    } & {
+      $value: UnwrapRef<TV>
+    } & {
+      toObject(): UnwrapRef<TV>
+    } & ValidationValue<TV> & { b: 1 }
+  : {
+      [K in keyof T]: K extends `$${infer _}`
+        ? UnwrapRef<T[K]>
+        : Validation<T[K]>;
+    } &
+    (NonDollarSign<keyof T> extends string
+      ? ValidationGroupResult & { toObject(): ToObjectOutput<T> }
+      : {})
+
+/* /Output */
+
+function isValidation(v: any): v is ValidationInputType<any, any> {
+  return typeof v.$value !== 'undefined'
+}
+
+function isValidatorObject(v: any): v is ValidatorObject<any> {
+  return isObject(v)
+}
+
+const buildValidationFunction = (
+  r: Ref<any>,
+  f: ValidatorFunc<any>,
+  m: Ref<string | undefined>,
+  handlers: Array<Function>,
+) => {
+  const $promise: Ref<Promise<boolean> | null> = ref(null)
+  const $pending = ref(false)
+  const $error = ref<Error | string | true>()
+  const $invalid = ref(false)
+  let context: any
+
+  const onChange = (r: any) => {
+    const p = async () => {
+      try {
+        $pending.value = true
+
+        const result = f(r, context)
+        if (isPromise(result))
+          $invalid.value = !(await result)
+        else
+          $invalid.value = !result
+
+        $error.value = $invalid.value ? m.value || true : undefined
+      }
+      catch (e) {
+        $invalid.value = true
+        throw e
+      }
+      finally {
+        $pending.value = false
+      }
+    }
+    $promise.value = p().catch((x) => {
+      $error.value = unwrap(x)
+      $invalid.value = true
+      return x
+    })
+  }
+
+  handlers.push((ctx: any) => {
+    context = ctx
+    watch(
+      () => {
+        try {
+          // keep track on the external dependencies
+          f(r.value, context)
+        }
+        catch (e) {
+          // ignore error
+        }
+        return r.value
+      },
+      onChange,
+      { deep: true, immediate: true },
+    )
+  })
+
+  function $touch() {
+    onChange(r.value)
+  }
+
+  return {
+    $promise,
+    $pending,
+    $invalid,
+    $error,
+
+    $touch,
+  }
+}
+
+const buildValidationValue = (
+  r: Ref<any>,
+  v: Validator<any>,
+  handlers: Array<Function>,
+): ValidatorResult & ValidatorResultPromise & ValidatorResultMessage => {
+  const { $message, $validator, ...$rest } = isValidatorObject(v)
+    ? v
+    : { $validator: v, $message: undefined }
+
+  const {
+    $pending,
+    $promise,
+    $invalid,
+    $error,
+    $touch,
+  } = buildValidationFunction(r, $validator, ref($message), handlers)
+
+  return {
+    $pending,
+    $error,
+    $promise,
+    $invalid,
+    $message,
+    $touch,
+    ...$rest,
+  } as any
+}
+
+const buildValidation = <T>(
+  o: ValidationInput<T>,
+  handlers: Array<Function>,
+): Record<string, Validation<any>> => {
+  const r: Record<string, Validation<any>> = {}
+  const $value: any | undefined = isValidation(o) ? wrap(o.$value) : undefined
+  for (const k of Object.keys(o)) {
+    if (k[0] === '$') {
+      if (k === '$value') {
+        r[k] = $value
+        const $dirty = ref(false)
+
+        let dirtyWatch = NO_OP
+        const createDirtyWatcher = () => {
+          dirtyWatch()
+
+          dirtyWatch = watch(
+            $value,
+            () => {
+              $dirty.value = true
+              dirtyWatch()
+            },
+            { immediate: false, deep: true },
+          )
+        }
+
+        createDirtyWatcher();
+
+        (r as any).$dirty = $dirty;
+        (r as any).$reset = () => {
+          $dirty.value = false
+
+          createDirtyWatcher()
+        };
+        (r as any).$touch = () => ($dirty.value = true)
+
+        // @ts-expect-error
+        r.toObject = () => unwrap($value)
+        continue
+      }
+      else {
+        r[k] = (o as any)[k]
+        continue
+      }
+    }
+
+    if ($value) {
+      const validation = buildValidationValue(
+        $value,
+        (o as Record<string, any>)[k],
+        handlers,
+      )
+
+      // @ts-expect-error no valid type
+      r[k] = validation
+    }
+    else {
+      const validation = buildValidation(
+        (o as Record<string, any>)[k],
+        handlers,
+      )
+
+      let $anyDirty: Ref<boolean> | undefined
+      let $errors: Readonly<Ref<Readonly<Array<any>>>>
+      let $anyInvalid: Ref<boolean>
+
+      let toObject: () => Record<string, any> = NO_OP as any
+
+      if (isValidation(validation)) {
+        const validations = Object.keys(validation)
+          .filter(x => x[0] !== '$')
+          .map(x => (validation[x] as any) as ValidatorResult)
+
+        $errors = computed(() =>
+          validations
+            .map(x => x.$error)
+            .map(x => unwrap(x))
+            .filter(x => x !== undefined),
+        )
+        // $anyDirty = computed(() => validations.some(x => !!x));
+        $anyInvalid = computed(() =>
+          validations.some((x) => {
+            return !!unwrap(x.$invalid)
+          }),
+        )
+
+        toObject = () => {
+          return Object.keys(validation)
+            .filter(x => x[0] !== '$')
+            .reduce((p, c) => {
+              // @ts-expect-error
+              p[c] = validation[c].toObject()
+              return p
+            }, {})
+        }
+      }
+      else {
+        const validations = Object.keys(validation).map(
+          x => (validation[x] as any) as ValidationGroupResult,
+        )
+        $errors = computed(() => {
+          return validations
+            .map(x => unwrap(x.$errors))
+            .filter(x => x !== undefined)
+            .filter((x) => {
+              return x.some(Boolean)
+            })
+        })
+        $anyDirty = computed(() => {
+          return validations.some((x) => {
+            return (
+              unwrap(x.$anyDirty)
+              || (isBoolean(unwrap((x as any).$dirty))
+                && unwrap((x as any).$dirty))
+            )
+          })
+        })
+        $anyInvalid = computed(() =>
+          validations.some((x) => {
+            return !!unwrap(x.$anyInvalid)
+          }),
+        )
+
+        toObject = () => {
+          return Object.keys(validation)
+            .filter(x => x[0] !== '$')
+            .reduce((p, c) => {
+              // @ts-expect-error
+              p[c] = validation[c].toObject()
+              return p
+            }, {})
+        }
+      }
+
+      r[k] = {
+        toObject,
+        ...validation,
+        $errors,
+        $anyInvalid,
+      } as any
+
+      if ($anyDirty) {
+        (r[k] as any).$anyDirty = $anyDirty
+
+        const keys = Object.keys(r[k]).filter(
+          x => x[0] !== '$' && isObject(r[k][x]),
+        )
+        r[k].$touch = () => {
+          // r[k].
+          keys.forEach((m) => {
+            const touch = r[k][m].$touch
+            if (touch)
+              touch()
+          })
+        }
+        r[k].$reset = () => {
+          keys.forEach((m) => {
+            const reset = r[k][m].$reset
+            if (reset)
+              reset()
+          })
+        }
+      }
+    }
+  }
+  return r
+}
+
+export function useValidation<T extends UseValidation<E>, E = any>(
+  input: E,
+): Validation<E> & ValidationGroupResult & { toObject(): ToObjectOutput<E> } {
+  const handlers: Array<Function> = []
+  const validation = buildValidation({ input }, handlers)
+  // convert to reactive, this will make it annoying to deconstruct, but
+  // allows to use it directly on the render template without `.value`
+  // https://github.com/vuejs/vue-next/pull/738
+
+  // @ts-expect-error TODO check this error
+  const validationInput = reactive(validation.input)
+  // set the context, this will allow to use this object as the second
+  // argument when calling validators
+  handlers.forEach(x => x(validationInput))
+
+  return validationInput as any
+}
---
+++ test/src/validation/validation.test.ts
@@ -0,0 +1,506 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { useValidation, NO_OP } from '@elonehoo/pistachio'
+import { ref } from 'vue'
+import { nextTick, createVue } from '../utils'
+
+describe("validation", () => {
+  it("validation", async () => {
+    const $value = ref("");
+    const $value1 = ref("");
+
+    const validation = useValidation({
+      test: {
+        $value,
+        required(x: string) {
+          return !!x;
+        },
+      },
+      test1: {
+        $value: $value1,
+        required(x: string) {
+          return !!x;
+        },
+      },
+    });
+
+    expect(validation).toMatchObject({
+      $anyDirty: false,
+      $anyInvalid: true,
+
+      test: {
+        $dirty: false,
+        $value: $value.value,
+        required: {
+          $invalid: true,
+        },
+      },
+      test1: {
+        $dirty: false,
+        $value: $value1.value,
+        required: {
+          $invalid: true,
+        },
+      },
+    });
+
+    $value.value = "hello";
+
+    await nextTick();
+
+    expect(validation.test.$value).toBe($value.value);
+    expect(validation).toMatchObject({
+      $anyDirty: true,
+      $anyInvalid: true,
+
+      test: {
+        $dirty: true,
+        $value: $value.value,
+        required: {
+          $invalid: false,
+        },
+      },
+      test1: {
+        $dirty: false,
+        $value: $value1.value,
+        required: {
+          $invalid: true,
+        },
+      },
+    });
+  });
+
+  it("validator should run if dependent of other ref", async () => {
+    const password = ref("");
+    const form = useValidation({
+      password: {
+        $value: password,
+      },
+      password2: {
+        $value: ref(""),
+        samePassword(r: string, ctx: any) {
+          return r === ctx.password.$value;
+        },
+      },
+    });
+
+    expect(form.password2.samePassword.$invalid).toBe(false);
+
+    form.password.$value = "test";
+    await nextTick();
+
+    expect(form.password2.samePassword.$invalid).toBe(true);
+  });
+
+  it("should keep values with key starting `$`", () => {
+    const $args = [1, 2];
+    const v = useValidation({
+      password: {
+        $value: ref(""),
+        $args,
+      },
+    });
+
+    expect(v.password.$args).toStrictEqual($args);
+  });
+
+  it("should store error if exception thrown ", async () => {
+    const error = new Error("my error");
+    const v = useValidation({
+      password: {
+        $value: ref(""),
+        required() {
+          throw error;
+        },
+      },
+    });
+
+    await nextTick();
+
+    expect(v.password.required.$error).toStrictEqual(error);
+    expect(v.password.$errors).toMatchObject([error]);
+    expect(v.$errors).toMatchObject([[error]]);
+  });
+
+  it("should handle promise validator", async () => {
+    let promiseResolve: Function = NO_OP;
+    const promise = new Promise<boolean>(
+      (resolve) => (promiseResolve = resolve)
+    );
+
+    const v = useValidation({
+      password: {
+        $value: ref(""),
+        required() {
+          return promise;
+        },
+      },
+    });
+
+    expect(v.password.required.$pending).toBe(true);
+
+    promiseResolve(true);
+    await nextTick();
+
+    expect(v.password.required.$pending).toBe(false);
+  });
+
+  it("should handle promise validator with objectValidator", async () => {
+    let promiseResolve: Function = NO_OP;
+    const promise = new Promise<boolean>(
+      (resolve) => (promiseResolve = resolve)
+    );
+
+    const v = useValidation({
+      password: {
+        $value: ref(""),
+        required: {
+          $validator() {
+            return promise;
+          },
+          $message: ref("Err"),
+        },
+      },
+    });
+
+    v.password.$value;
+    expect(v.password.required.$pending).toBe(true);
+    expect(v.password.required.$message).toBe("Err");
+
+    promiseResolve(true);
+    await nextTick();
+
+    expect(v.password.required.$pending).toBe(false);
+    expect(v.password.required.$message).toBe("Err");
+  });
+
+  it("should test error ", async () => {
+    const v = useValidation({
+      input: {
+        $value: "",
+
+        required() {
+          throw Error("error 1");
+        },
+        match() {
+          throw Error("error 2");
+        },
+      },
+    });
+    v.input.$value = "1";
+    await nextTick();
+
+    expect(v.input.$errors).toStrictEqual([
+      new Error("error 1"),
+      new Error("error 2"),
+    ]);
+  });
+
+  it("should store $errors if $message is available", () => {
+    const $message = "Invalid value";
+    const v = useValidation({
+      input: {
+        $value: "",
+        required: {
+          $validator(x: string) {
+            return false;
+          },
+          $message,
+        },
+      },
+      otherInput: {
+        $value: "",
+        required(x: string) {
+          return false;
+        },
+      },
+    });
+
+    expect(v.input.$errors).toMatchObject([$message]);
+    expect(v.otherInput.$errors).toMatchObject([true]);
+  });
+
+  describe("object", () => {
+    it("should convert to object", () => {
+      const v = useValidation({
+        input: {
+          $value: "",
+          required: {
+            $validator(x: string) {
+              return false;
+            },
+            $message: "test",
+          },
+        },
+        otherInput: {
+          $value: "",
+          required(x: string) {
+            return false;
+          },
+        },
+      });
+
+      expect(v.toObject()).toMatchObject({ input: "", otherInput: "" });
+
+      v.input.$value = "test";
+      v.otherInput.$value = "other";
+
+      expect(v.toObject()).toMatchObject({
+        input: "test",
+        otherInput: "other",
+      });
+
+      expect(v.input.toObject()).toBe("test");
+    });
+
+    it("should convert to object even on nested validations", () => {
+      const v = useValidation({
+        address: {
+          address1: {
+            $value: "address1",
+          },
+          address2: {
+            part1: {
+              $value: "part1",
+            },
+            part2: {
+              $value: "part2",
+            },
+          },
+        },
+      });
+      expect(v.toObject()).toMatchObject({
+        address: {
+          address1: "address1",
+          address2: {
+            part1: "part1",
+            part2: "part2",
+          },
+        },
+      });
+
+      v.address.address2.part1.$value = "1";
+
+      expect(v.toObject()).toMatchObject({
+        address: {
+          address1: "address1",
+          address2: {
+            part1: "1",
+            part2: "part2",
+          },
+        },
+      });
+
+      expect(v.address.toObject()).toMatchObject({
+        address1: "address1",
+        address2: {
+          part1: "1",
+          part2: "part2",
+        },
+      });
+
+      expect(v.address.address1.toObject()).toBe(v.address.address1.$value);
+
+      expect(v.address.address2.toObject()).toMatchObject({
+        part1: "1",
+        part2: "part2",
+      });
+    });
+
+    it("should not keep keys starting with '$'", () => {
+      const v = useValidation({
+        $test: "ddd",
+        age: {
+          $value: 20,
+        },
+      }).toObject();
+
+      expect(v.$test).toBe(undefined);
+      expect(v).toStrictEqual({
+        age: 20,
+      });
+    });
+  });
+
+  describe("render", () => {
+    it("should show error", async () => {
+      const required = (x: any) => !!x;
+      let form = useValidation({
+        firstName: {
+          $value: ref(""),
+          required,
+          otherRequired: {
+            $validator: required,
+            $message: ref("password is required"),
+          },
+        },
+      });
+
+      const { mount } = createVue({
+        template: `
+        <div>
+          <input name="firstName" v-model="form.firstName.$value" placeholder="firstName" />
+
+          <p id="form-anyInvalid" v-if="form.$anyInvalid">Invalid</p>
+          <p id="form-dirty-anyError" v-if="form.$anyDirty && form.$anyInvalid">Dirty & Invalid</p>
+          <p id="firstname-error-required" v-if="form.firstName.$dirty && form.firstName.required.$invalid">
+            Invalid
+          </p>
+          <p id="firstname-error" v-if="form.firstName.$dirty && form.firstName.otherRequired.$invalid">
+            {{ form.firstName.otherRequired.$message }}
+          </p>
+        </div>
+        `,
+        setup() {
+          return {
+            form,
+          };
+        },
+      });
+
+      const vm = mount();
+      await nextTick();
+
+      const input = document.getElementsByName("firstName")[0];
+
+      const anyInvalid = () => document.getElementById("form-anyInvalid");
+      const dirtyInvalid = () => document.getElementById("form-dirty-anyError");
+      const nameRequired = () =>
+        document.getElementById("firstname-error-required");
+      const otherRequired = () => document.getElementById("firstname-error");
+
+      vm.$el;
+
+      expect(anyInvalid()).not.toBeNull();
+      expect(dirtyInvalid()).toBeNull();
+      expect(nameRequired()).toBeNull();
+      expect(otherRequired()).toBeNull();
+
+      expect((input as any).value).toBe(form.firstName.$value);
+
+      (input as any).value = "hello world";
+      input.dispatchEvent(new InputEvent("input"));
+      await nextTick();
+
+      expect(form.firstName.$value).toBe("hello world");
+
+      expect(dirtyInvalid()).toBeNull();
+      expect(nameRequired()).toBeNull();
+      expect(otherRequired()).toBeNull();
+      expect(anyInvalid()).toBeNull();
+
+      (input as any).value = "";
+      input.dispatchEvent(new InputEvent("input"));
+      await nextTick();
+
+      expect(dirtyInvalid()).not.toBeNull();
+      expect(nameRequired()).not.toBeNull();
+      expect(otherRequired()).not.toBeNull();
+      expect(anyInvalid()).not.toBeNull();
+
+      expect(otherRequired()!.textContent!.trim()).toBe(
+        form.firstName.otherRequired.$message
+      );
+    });
+  });
+
+  describe("touch/reset", () => {
+    it("should run validation", () => {
+      const required = (x: any) => !!x;
+      const v = useValidation({
+        test: {
+          $value: "",
+          required,
+        },
+        deep: {
+          v1: {
+            $value: "",
+            required,
+          },
+          v2: {
+            $value: "",
+            required,
+          },
+        },
+      });
+
+      expect(v.$anyDirty).toBe(false);
+
+      expect(v.test.$dirty).toBe(false);
+      expect(v.test.$anyInvalid).toBe(true);
+      v.test.$touch();
+      expect(v.test.$dirty).toBe(true);
+      v.test.$reset();
+      expect(v.test.$dirty).toBe(false);
+
+      expect(v.deep.v1.$dirty).toBe(false);
+      expect(v.deep.v2.$dirty).toBe(false);
+      v.deep.$touch();
+      expect(v.deep.v1.$dirty).toBe(true);
+      expect(v.deep.v2.$dirty).toBe(true);
+      v.deep.$reset();
+      expect(v.deep.v1.$dirty).toBe(false);
+      expect(v.deep.v2.$dirty).toBe(false);
+
+      v.$touch();
+      expect(v.$anyDirty).toBe(true);
+      expect(v.test.$dirty).toBe(true);
+      expect(v.deep.v1.$dirty).toBe(true);
+      expect(v.deep.v2.$dirty).toBe(true);
+      v.$reset();
+      expect(v.test.$dirty).toBe(false);
+      expect(v.deep.v1.$dirty).toBe(false);
+      expect(v.deep.v2.$dirty).toBe(false);
+      expect(v.$anyDirty).toBe(false);
+    });
+
+    it("should update $dirty after $reset on update value", async () => {
+      const required = (x: any) => !!x;
+
+      const $testValue = ref("");
+      const $deepV1Value = ref("");
+      const $deepV2Value = ref("");
+
+      const v = useValidation({
+        test: {
+          $value: $testValue,
+          required,
+        },
+        deep: {
+          v1: {
+            $value: $deepV1Value,
+            required,
+          },
+          v2: {
+            $value: $deepV2Value,
+            required,
+          },
+        },
+      });
+
+      expect(v.$anyDirty).toBe(false);
+
+      expect(v.test.$dirty).toBe(false);
+      expect(v.test.$anyInvalid).toBe(true);
+      $testValue.value += "test";
+
+      await nextTick();
+
+      expect(v.test.$dirty).toBe(true);
+      v.test.$reset();
+      expect(v.test.$dirty).toBe(false);
+
+      expect(v.deep.v1.$dirty).toBe(false);
+      expect(v.deep.v2.$dirty).toBe(false);
+      $deepV1Value.value += "test";
+      $deepV2Value.value += "test";
+
+      await nextTick();
+
+      expect(v.deep.v1.$dirty).toBe(true);
+      expect(v.deep.v2.$dirty).toBe(true);
+      v.deep.$reset();
+      expect(v.deep.v1.$dirty).toBe(false);
+      expect(v.deep.v2.$dirty).toBe(false);
+    });
+  });
+});
