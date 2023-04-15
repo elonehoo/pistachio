918dddd0c58ea508fd6a72a30239ce605ee75da9
elonehoo <hi@elonehoo.me>
2023-04-15 19:35:57 +0800

    chore: update all non-major dependencies

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,37 +1,11 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-import types from '@elonehoo/vite-plugin-type-ts'
-import recordTime from '@elonehoo/vite-plugin-record-time'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-  plugins: [
-    types({
-      tsConfigFilePath: './tsconfig.json',
-      cleanVueFileName: true,
-      insertTypesEntry: true,
-    }),
-    recordTime(),
-  ],
-  build: {
-    outDir: 'dist',
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'vue-hooks',
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
     },
   },
 })
